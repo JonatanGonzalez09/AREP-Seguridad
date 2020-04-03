c3f23bae8da405c846fa49823ba0b3d21a8398eb
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-04-03 15:57:30 -0500

    Latex y Documentacion

--- src/main/java/edu/escuelaing/arep/app/model/User.java
+++ src/main/java/edu/escuelaing/arep/app/model/User.java
@@ -9,9 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
 
 @Entity
 @Table(name="usuarios")
